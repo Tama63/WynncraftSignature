3f7f7da0a56f7737505eb9eb19e8d82947bc69f0
Chris Ireland <ireland63@googlemail.com>
2013-11-05 18:21:07 +0000

    Pulls from local merc

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+/* 
+	Wynncraft Signature Generator 
+
+	Copyright Wynncraft 2013
+	@author Chris Ireland <ireland63@gmail.com>
+
+	@file Functions
+*/
+
+// Simple Sanitizier
+function protectInput($protectinput){
+	$protectinput = stripslashes($protectinput);
+	$protectinput = str_replace(array("<", ">", "!", "?", "(", ")"), "", $protectinput);
+
+	return $protectinput;
+}
+
+// Tidy num
+function convertNum($numrev) {
+	if($numrev >= 1000 && $numrev < 1000000) {
+		$num = $numrev /1000;
+		$num = number_format((float)$num, 1, ".", "");
+		$num = $num."k";
+	} elseif($numrev >= 1000000) {
+		$num = $numrev /1000000;
+		$num = number_format((float)$num, 1, ".", "");
+		$num = $num."m";
+	} else {
+		$num = $numrev;
+	}
+	             
+	return $num;
+}
